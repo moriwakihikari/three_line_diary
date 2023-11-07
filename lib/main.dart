c0d2a716e6a0c5a0075b0c033b097a31bb7e8ab4
cin-moriwaki <moriwaki-h@cin-group.com>
2023-11-07 09:55:51 +0900

    ログイン追加

--- lib/main.dart
+++ lib/main.dart
@@ -116,7 +116,35 @@ class LoginPageState extends State<LoginPage> {
                     }
                   },
                 ),
-              )
+              ),
+              const SizedBox(height: 0),
+              Container(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                      child: Text('ログイン'),
+                      onPressed: () async {
+                        try {
+                          // メール/パスワードでログイン
+                          final FirebaseAuth auth = FirebaseAuth.instance;
+                          final result = await auth.signInWithEmailAndPassword(
+                              email: email, password: password);
+
+                          // ユーザー情報を更新
+                          // userState.setUser(result.user!);
+                          // ログインに成功した場合
+                          // チャット画面に遷移＋ログイン画面を破棄
+                          await Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) {
+                              return ThreeLineDiaryPage();
+                            }),
+                          );
+                        } catch (e) {
+                          // ログインに失敗した場合
+                          setState(() {
+                            infoText = "ログインに失敗しました:(e.toString())";
+                          });
+                        }
+                      }))
             ],
           ),
         ),
